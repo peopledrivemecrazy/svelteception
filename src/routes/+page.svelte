7c4496541104ee8653232d0c6038192a664b6803
shriji <shrijike@gmail.com>
2023-02-24 12:57:52 -0500

    ++

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import code from './+page.svelte?raw';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<code>
+	<pre>{code}</pre>
+</code>
+
+<style>
+	pre {
+		width: fit-content;
+		padding: 12px;
+		background-color: black;
+		color: white;
+		border-radius: 4px
+	}
+</style>
